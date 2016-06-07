0e48bcd6bdff4d97d1c7ccfb06499b9398310767
Kerry Jiang <kerry-jiang@hotmail.com>
2016-06-07 16:40:52 +0800

    tried to use dependency injection

--- src/NRPC.Client/Extensions.cs
+++ src/NRPC.Client/Extensions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Reflection;
 using Microsoft.Extensions.DependencyInjection;
+using NRPC.Base;
 
 namespace NRPC.Client
 {
@@ -15,6 +16,18 @@ namespace NRPC.Client
             var proxyType = ClientDispatchProxy.GetPorxyType<T, ClientDispatchProxy>();
 
             return services.AddTransient(typeof(T), proxyType);
+        }
+
+        public static IServiceCollection AddCodec<T>(this ServiceCollection services)
+            where T : class, IRpcCodec
+        {
+            return services.AddTransient<IRpcCodec, T>();
+        }
+
+        public static IServiceCollection AddChannel<T>(this ServiceCollection services)
+            where T : class, IRpcChannel
+        {
+            return services.AddTransient<IRpcChannel, T>();
         }
     }
 }
